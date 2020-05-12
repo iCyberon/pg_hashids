be41225b9c585c18c4db74546731659a7654c9ae
Dmitry <dmitry.wagin@ya.ru>
2020-05-12 18:18:39 +0300

    added error handling

--- pg_hashids.c
+++ pg_hashids.c
@@ -17,6 +17,29 @@ PG_FUNCTION_INFO_V1(id_encode_array);
 PG_FUNCTION_INFO_V1(id_decode);
 PG_FUNCTION_INFO_V1(id_decode_once);
 
+static void hashids_error()
+{
+  switch (hashids_errno) {
+    case HASHIDS_ERROR_ALLOC:
+      ereport(ERROR, (errcode(ERRCODE_OUT_OF_MEMORY), errmsg("allocation failed")));
+      break;
+    case HASHIDS_ERROR_ALPHABET_LENGTH:
+      ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg("alphabet is too short")));
+      break;
+    case HASHIDS_ERROR_ALPHABET_SPACE:
+      ereport(ERROR,
+              (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+                  errmsg("alphabet contains whitespace characters")));
+      break;
+    case HASHIDS_ERROR_INVALID_HASH:
+      ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg("invalid hash")));
+      break;
+    default:
+      ereport(ERROR, (errcode(ERRCODE_EXTERNAL_ROUTINE_EXCEPTION), errmsg("unknown error")));
+      break;
+  }
+}
+
 // based on new_intArrayType contrib/intarray/_int_tool.c
 static ArrayType *new_intArrayType(int num)
 {
@@ -27,7 +50,7 @@ static ArrayType *new_intArrayType(int num)
 
   SET_VARSIZE(r, nbytes);
   ARR_NDIM(r) = 1;
-  r->dataoffset = 0;			/* marker for no null bitmap */
+  r->dataoffset = 0;  /* marker for no null bitmap */
   ARR_ELEMTYPE(r) = INT8OID;
   ARR_DIMS(r)[0] = num;
   ARR_LBOUND(r)[0] = 1;
@@ -54,9 +77,13 @@ id_encode(PG_FUNCTION_ARGS)
   } else if (PG_NARGS() == 3) {
     hashids = hashids_init2(text_to_cstring(PG_GETARG_TEXT_P(1)), PG_GETARG_INT32(2));
   } else if (PG_NARGS() == 4) {
-    hashids = hashids_init3(text_to_cstring(PG_GETARG_TEXT_P(1)), PG_GETARG_INT32(2), text_to_cstring(PG_GETARG_TEXT_P(3)));
+    hashids = hashids_init3(text_to_cstring(PG_GETARG_TEXT_P(1)), PG_GETARG_INT32(2),
+                            text_to_cstring(PG_GETARG_TEXT_P(3)));
   } else {
     hashids = hashids_init(NULL);
+  }
+  if (!hashids) {
+    hashids_error();
   }
 
   hash = palloc0(hashids_estimate_encoded_size(hashids, 1, (unsigned long long *) &number));
@@ -87,16 +114,23 @@ id_encode_array(PG_FUNCTION_ARGS)
   numbers_count = ARR_DIMS(numbers)[0];
 
   if (array_contains_nulls(numbers)) {
-    PG_RETURN_NULL();
+    ereport(ERROR,
+            (errcode(ERRCODE_NULL_VALUE_NOT_ALLOWED),
+                errmsg("null value not allowed for array element")));
+
   }
   if (PG_NARGS() == 2) {
     hashids = hashids_init2(text_to_cstring(PG_GETARG_TEXT_P(1)), 0);
   } else if (PG_NARGS() == 3) {
     hashids = hashids_init2(text_to_cstring(PG_GETARG_TEXT_P(1)), PG_GETARG_INT32(2));
   } else if (PG_NARGS() == 4) {
-    hashids = hashids_init3(text_to_cstring(PG_GETARG_TEXT_P(1)), PG_GETARG_INT32(2), text_to_cstring(PG_GETARG_TEXT_P(3)));
+    hashids = hashids_init3(text_to_cstring(PG_GETARG_TEXT_P(1)), PG_GETARG_INT32(2),
+                            text_to_cstring(PG_GETARG_TEXT_P(3)));
   } else {
     hashids = hashids_init(NULL);
+  }
+  if (!hashids) {
+    hashids_error();
   }
 
   hash = palloc0(hashids_estimate_encoded_size(hashids, numbers_count, (unsigned long long *) ARR_DATA_PTR(numbers)));
@@ -124,13 +158,21 @@ id_decode(PG_FUNCTION_ARGS)
   } else if (PG_NARGS() == 3) {
     hashids = hashids_init2(text_to_cstring(PG_GETARG_TEXT_P(1)), PG_GETARG_INT32(2));
   } else if (PG_NARGS() == 4) {
-    hashids = hashids_init3(text_to_cstring(PG_GETARG_TEXT_P(1)), PG_GETARG_INT32(2), text_to_cstring(PG_GETARG_TEXT_P(3)));
+    hashids = hashids_init3(text_to_cstring(PG_GETARG_TEXT_P(1)), PG_GETARG_INT32(2),
+                            text_to_cstring(PG_GETARG_TEXT_P(3)));
   } else {
     hashids = hashids_init(NULL);
+  }
+  if (!hashids) {
+    hashids_error();
   }
 
   hash = text_to_cstring(PG_GETARG_TEXT_P(0));
   numbers_count = hashids_numbers_count(hashids, hash);
+  if (!numbers_count) {
+    hashids_error();
+  }
+
   numbers = palloc0(numbers_count * sizeof(int64));
   hashids_decode(hashids, hash, (unsigned long long *) numbers, numbers_count);
 
@@ -160,16 +202,21 @@ id_decode_once(PG_FUNCTION_ARGS)
   } else if (PG_NARGS() == 3) {
     hashids = hashids_init2(text_to_cstring(PG_GETARG_TEXT_P(1)), PG_GETARG_INT32(2));
   } else if (PG_NARGS() == 4) {
-    hashids = hashids_init3(text_to_cstring(PG_GETARG_TEXT_P(1)), PG_GETARG_INT32(2), text_to_cstring(PG_GETARG_TEXT_P(3)));
+    hashids = hashids_init3(text_to_cstring(PG_GETARG_TEXT_P(1)), PG_GETARG_INT32(2),
+                            text_to_cstring(PG_GETARG_TEXT_P(3)));
   } else {
     hashids = hashids_init(NULL);
+  }
+  if (!hashids) {
+    hashids_error();
   }
 
   hash = text_to_cstring(PG_GETARG_TEXT_P(0));
   numbers_count = hashids_numbers_count(hashids, hash);
-  if (numbers_count == 0) {
-    PG_RETURN_NULL();
+  if (!numbers_count) {
+    hashids_error();
   }
+
   hashids_decode(hashids, hash, (unsigned long long *) &number, 1);
 
   hashids_free(hashids);
